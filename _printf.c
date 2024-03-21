7d48c015f5e532815aec9d8cfb774d76e5b356f4
Joel Caban <joel.caban2017@gmail.com>
2024-03-21 11:20:12 -0400

    handle_format

--- _printf.c
+++ _printf.c
@@ -1,51 +1,59 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
+
 /**
- *_printf - produces output according to a format
+ * handle_format - Handles formatting for specific specifiers.
+ * @specifier: The format specifier to handle.
+ * @args: The list of arguments passed to the function.
+ *
+ * Return: The number of characters printed by the handling function,
+ * or 2 if an unsupported specifier is encountered.
+ */
+int handle_format(char specifier, va_list args)
+{
+	switch (specifier)
+	{
+		case 'c': return (print_char(args));
+		case 's': return (print_string(args));
+		case '%': return (print_percent(args));
+		case 'd':
+		case 'i': return (print_int(args));
+		default:
+				  _putchar('%');
+				  _putchar(specifier);
+				return (2);
+	}
+}
+
+/**
+ * _printf - produces output according to a format
  *@format: character string composed of zero or more directives
  *
  *Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-	int i, count;
+	int i = 0, count = 0;
 	va_list args;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 
-	i = 0;
-	count = 0;
 	va_start(args, format);
-	
-	while (format[i] != '\0')
+
+	while (format && format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
+			count += handle_format(format[i + 1], args);
 			i++;
-			if (format[i] == '\0')
-			{
-				_putchar('%');
-				count++;
-				break;
-			}
-			else if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == 's')
-				count += print_string(args);
-			else if (format[i] == '%')
-				count += print_percent(args);
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_int(args);
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
+		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			_putchar('%');
+			count++;
+			break;
 		}
 		else
 		{
@@ -54,6 +62,7 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
+
 	va_end(args);
 	return (count);
 }
